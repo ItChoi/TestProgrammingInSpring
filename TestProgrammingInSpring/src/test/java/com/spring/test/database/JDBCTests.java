6688de88e08c4b156bc2e1012c1f955c63509485
itchoi <itchoi0429@gmail.com>
2019-12-27 19:37:53 +0900

    DB Connection Test

--- TestProgrammingInSpring/src/test/java/com/spring/test/database/JDBCTests.java
+++ TestProgrammingInSpring/src/test/java/com/spring/test/database/JDBCTests.java
@@ -1,7 +1,11 @@
 package com.spring.test.database;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -12,21 +16,33 @@ class JDBCTests {
 	public static final Logger log = LoggerFactory.getLogger(JDBCTests.class);
 	
 	static {
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-		} catch(Exception e) {
-			log.info("에러 메시지: {}", e);
-		}
+		/* 
+		 * 클래스 'com.mysql.jdbc.Driver'로드 중 더 이상 사용되지 않습니다.
+		 * 새로운 드라이버 클래스는`com.mysql.cj.jdbc.Driver '입니다. 
+		 * 드라이버는 SPI를 통해 자동으로 등록되며 드라이버 클래스의 수동로드는 일반적으로 필요하지 않습니다.
+		 * try { Class.forName("com.mysql.jdbc.Driver"); } catch(Exception e) {
+		 * log.info("에러 메시지: {}", e); }
+		 */
 	}
 
+	
+	
 	@Test
 	void testConnection() {
+		
 		try {
+			// TODO 테스트 더하기
+			Properties properties = new Properties();
+			FileReader resources = new FileReader("/resources/properties/config.properties");
+			
+			properties.load(resources);
+			
 			Connection con = DriverManager.getConnection(
-						"jdbc:oracle:thin:@localhost:1521:orcl",
-						"asdasd",
-						"asdasd"
-					);
+					properties.getProperty("jdbc.url"),
+					properties.getProperty("jdbc.username"),
+					properties.getProperty("jdbc.password")
+			);
+			
 			
 		} catch(Exception e) {
 			log.info("에러 메시지: {}", e);
