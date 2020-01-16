675b2d2124745629ba82359d1a872d71c6a3d3ef
itchoi <itchoi0429@gmail.com>
2020-01-16 13:18:24 +0900

    객체지향 설계 의존성 관리 공부

---
+++ TestProgrammingInSpring/src/test/java/com/spring/test/regex/RegularExpressionTest.java
@@ -0,0 +1,17 @@
+package com.spring.test.regex;
+
+import org.junit.jupiter.api.Test;
+
+public class RegularExpressionTest {
+	
+	@Test
+	public void 한글체크_정규식_테스트() {
+		String useHangeul = "asdasd한글sadasdasd";
+		String disUseHangeul = "sadasdasdasdasd";
+		
+		
+		
+		
+	}
+
+}
