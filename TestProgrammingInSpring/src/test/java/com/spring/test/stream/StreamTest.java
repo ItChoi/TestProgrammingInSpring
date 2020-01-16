8fc4b1e51b1b3c6b767e524ed455a2f2eb74742d
itchoi <itchoi0429@gmail.com>
2020-01-16 13:17:58 +0900

    객체지향 설계 공부 의존성 관리

--- TestProgrammingInSpring/src/test/java/com/spring/test/stream/StreamTest.java
+++ TestProgrammingInSpring/src/test/java/com/spring/test/stream/StreamTest.java
@@ -1,6 +1,10 @@
 package com.spring.test.stream;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 
@@ -38,6 +42,32 @@ public class StreamTest {
 				.filter(i -> i == "일")
 				.forEach(System.out::println);
 	}
+	
+	@Test
+	public void 테스트_코드() {
+		int[] a = {1,5,2,5,4,3};
+		Arrays.sort(a);
+		
+		Object aaa = Collectors.toList();
+		Arrays.stream(a).forEach(System.out::println);
+	}
+	
+	@Test
+	public void Arrays_테스트() {
+		int[] a = {1,5,2,5,4,3};
+		
+		System.out.println(Arrays.binarySearch(a, 22));
+	}
+	
+	@Test
+	public void 스트림_테스트05() {
+		String[] arr = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
+		
+		Stream<String> stream = Arrays.stream(arr, 1, 3);
+		
+		stream.forEach(System.out::println);
+	}
+	
 	
 }
 
